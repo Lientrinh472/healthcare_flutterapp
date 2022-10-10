faa6eea13926396aba59ea397972a950e544c546
Lien Trinh <trinhlq472@gmail.com>
2022-10-10 17:12:26 +0900

    Login screen implementation

---
+++ lib/login_page.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() =>_LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+          Image.asset('assets/images/logo.png'),
+          SizedBox(height: 50,),
+          // Text(
+          //   'Hello Nghia',
+          //   style: TextStyle(
+          //     fontWeight: FontWeight.bold,
+          //     fontSize: 24,
+          //   )
+          //   ),
+          // SizedBox(height: 30),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal:30),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xffF0F0F0),
+                borderRadius: BorderRadius.circular(12)
+              ),
+            child: Padding(
+              padding: const EdgeInsets.only(left:20),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 20),
+                  border: InputBorder.none,
+                  hintText: 'Username',
+                ),
+              ),
+            )
+            ),
+          ),
+
+          SizedBox(height: 20),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal:30),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xffF0F0F0),
+                borderRadius: BorderRadius.circular(12)
+              ),
+            child: Padding(
+              padding: const EdgeInsets.only(left:20),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 20),
+                  border: InputBorder.none,
+                  hintText: 'Password',
+                ),
+              ),
+            )
+            ),
+          ),
+           SizedBox(height: 20),
+
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 30),
+             child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color(0xff079D49),
+                borderRadius: BorderRadius.circular(55)
+              ),
+              child: Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    ),
+                ),
+              ),
+              ),
+           )
+        ]),
+      ),
+    ) ,
+  );
+}
+}
