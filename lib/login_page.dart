2b0c70b2e81e1c4c8e6e7b80515ac2b1604df98b
Lien Trinh <trinhlq472@gmail.com>
2022-10-13 23:07:12 +0900

    insert clickable button

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,100 +2,95 @@
 
 import 'package:flutter/material.dart';
 
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() =>_LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-          Image.asset('assets/images/logo.png'),
-          SizedBox(height: 50,),
-          // Text(
-          //   'Hello Nghia',
-          //   style: TextStyle(
-          //     fontWeight: FontWeight.bold,
-          //     fontSize: 24,
-          //   )
-          //   ),
-          // SizedBox(height: 30),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal:30),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xffF0F0F0),
-                borderRadius: BorderRadius.circular(12)
-              ),
-            child: Padding(
-              padding: const EdgeInsets.only(left:20),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(vertical: 20),
-                  border: InputBorder.none,
-                  hintText: 'Username',
-                ),
-              ),
-            )
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Image.asset('assets/images/logo.png'),
+            SizedBox(
+              height: 50,
             ),
-          ),
+            // Text(
+            //   'Hello Nghia',
+            //   style: TextStyle(
+            //     fontWeight: FontWeight.bold,
+            //     fontSize: 24,
+            //   )
+            //   ),
+            // SizedBox(height: 30),
 
-          SizedBox(height: 20),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal:30),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xffF0F0F0),
-                borderRadius: BorderRadius.circular(12)
-              ),
-            child: Padding(
-              padding: const EdgeInsets.only(left:20),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(vertical: 20),
-                  border: InputBorder.none,
-                  hintText: 'Password',
-                ),
-              ),
-            )
-            ),
-          ),
-           SizedBox(height: 20),
-
-           Padding(
-             padding: const EdgeInsets.symmetric(horizontal: 30),
-             child: Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Color(0xff079D49),
-                borderRadius: BorderRadius.circular(55)
-              ),
-              child: Center(
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xffF0F0F0),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 20),
+                        border: InputBorder.none,
+                        hintText: 'Username',
+                      ),
                     ),
+                  )),
+            ),
+
+            SizedBox(height: 20),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xffF0F0F0),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 20),
+                        border: InputBorder.none,
+                        hintText: 'Password',
+                      ),
+                    ),
+                  )),
+            ),
+            SizedBox(height: 20),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(354, 55),
+                    textStyle: TextStyle(fontSize: 16),
+                    foregroundColor: Colors.white,
+                    backgroundColor: Color(0xff079D49),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50), // <-- Radius
+                    ),
+                  ),
+                  child: Text('Login'),
+                  onPressed: (() {}),
                 ),
               ),
-              ),
-           )
-        ]),
+            ),
+          ]),
+        ),
       ),
-    ) ,
-  );
-}
+    );
+  }
 }
