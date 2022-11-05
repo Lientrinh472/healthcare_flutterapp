6def5152ea875e8334db91baefc3cdd057180a93
Lien Trinh <trinhlq472@gmail.com>
2022-11-05 23:56:00 +0900

    Implement 2nd row (log out)

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,6 +10,9 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
+    final degrees = 90;
+    final angle = degrees * pi / 180;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text(
@@ -54,6 +59,41 @@ class _ProfilePageState extends State<ProfilePage> {
                           fontSize: 18,
                           fontWeight: FontWeight.bold,
                           color: Colors.black)),
+                ]),
+              ),
+            ),
+            SizedBox(height: 7),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Container(
+                height: 50,
+                width: 354,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Row(children: [
+                  const SizedBox(width: 20),
+                  Transform.rotate(
+                      angle: angle,
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/logouticon.png'))),
+                      )),
+                  const SizedBox(width: 15),
+                  const Text('Log out',
+                      style: TextStyle(fontSize: 17, color: Colors.black)),
                 ]),
               ),
             ),
